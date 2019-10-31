c2d1477f49203f9211d352a30a50234e857a37bc
Wayne Ohmer <wayneohmer@gmail.com>
2019-10-31 13:53:06 -0700

    Initial Commit

---
+++ Character OrganizerTests/Character_OrganizerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Character_OrganizerTests.swift
+//  Character OrganizerTests
+//
+//  Created by Wayne Ohmer on 10/31/19.
+//  Copyright © 2019 Tryal by Fyre. All rights reserved.
+//
+
+import XCTest
+@testable import Character_Organizer
+
+class Character_OrganizerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
