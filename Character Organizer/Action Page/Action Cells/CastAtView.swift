04fbe01c1bed9226a6bd56c73d52aea11f6527f4
Wayne Ohmer <wayne@ohmer.me>
2020-07-19 15:16:21 -0700

    spell casting functional

---
+++ Character Organizer/Action Page/Action Cells/CastAtView.swift
@@ -0,0 +1,21 @@
+//
+//  CastAtView.swift
+//  Character Organizer
+//
+//  Created by Wayne Ohmer on 7/19/20.
+//  Copyright © 2020 Tryal by Fyre. All rights reserved.
+//
+
+import SwiftUI
+
+struct CastAtView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CastAtView_Previews: PreviewProvider {
+    static var previews: some View {
+        CastAtView()
+    }
+}
