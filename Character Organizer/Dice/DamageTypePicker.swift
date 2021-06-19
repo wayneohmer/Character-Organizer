894ffec92ad263cbe653da2e06f1335271c5860e
Wayne Ohmer <wayne@ohmer.me>
2021-06-19 10:50:16 -0700

    progress

---
+++ Character Organizer/Dice/DamageTypePicker.swift
@@ -0,0 +1,21 @@
+//
+//  DamageTypePicker.swift
+//  Character Organizer
+//
+//  Created by Wayne Ohmer on 4/9/21.
+//  Copyright © 2021 Tryal by Fyre. All rights reserved.
+//
+
+import SwiftUI
+
+struct DamageTypePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DamageTypePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        DamageTypePicker()
+    }
+}
